3bdd425ff8a91d71aea4e2471af2c616a5dddcfd
Ryan <ryancastles33@gmail.com>
2020-07-13 23:42:15 +1000

    Updates

---
+++ precache-manifest.98ca2720b0e0454921eae3bca23fdc1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0470543e143fd677201c449f28813e12",
+    "url": "/giphy-search-react/index.html"
+  },
+  {
+    "revision": "55f9fd70361a17a9c18d",
+    "url": "/giphy-search-react/static/css/main.79354d79.chunk.css"
+  },
+  {
+    "revision": "c3b4fe57be5d1e911012",
+    "url": "/giphy-search-react/static/js/2.b841cc44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy-search-react/static/js/2.b841cc44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f9fd70361a17a9c18d",
+    "url": "/giphy-search-react/static/js/main.4b8b6ba3.chunk.js"
+  },
+  {
+    "revision": "724d89485f82608c2197",
+    "url": "/giphy-search-react/static/js/runtime-main.1d2a8bf8.js"
+  }
+]);
